bec6da18af16aff28b034869ea9275080538da8f
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2022-05-22 17:08:11 +0100

    Mise en place de redux pour avoir un state manager

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initalState";
+import reducer from "./context/reducer";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </Router>
 );
 
